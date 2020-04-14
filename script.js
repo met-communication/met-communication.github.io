eb524b063827e834d1c89decb936b3f2377cfb96
gciandro13 <gciandrini@gmail.com>
2020-04-14 12:39:18 +0200

    Edits

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 
 $(document).ready(function(){
-console.log("hello");
-console.log("hello");
 
-
-$("#buttonProva").click(function(){
+$("#buttonPrint").click(function(){
     console.log("button pressed");
     var textInput = $("#inputUser").val();
     $("#outputUser").val(textInput); 
