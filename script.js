1efe58fae448c9a981f4ae2771c650de4ccbd672
gciandro13 <gciandrini@gmail.com>
2020-05-06 12:16:00 +0200

    Video cut at 27.36 and handle player youtube from jquery

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 
+        
+        var stopPlayAt = 1657; // Stop play at time in seconds
+        var stopPlayTimer;   // Reference to settimeout call
+        var startAt =257;
+        // This code loads the IFrame Player API code asynchronously.
+        var tag = document.createElement("script");
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName("script")[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      
+        // This function creates an <iframe> (and YouTube player)
+        // after the API code downloads.
+        var player;
+
+
+        function onYouTubeIframeAPIReady() {
+          player = new YT.Player("player", {
+            "height": "315",
+            "width": "560",
+            "videoId": "ln-CeHYkQvg",
+            "playerVars": {
+                start: startAt,
+                rel:0
+            },
+            "events": {
+              "onStateChange": onPlayerStateChange
+            }
+          });
+        }
+      
+
+          // The API will call this function when the video player is ready.
+        // This automatically starts the video playback when the player is loaded.
+
+        
+          // The API calls this function when the player's state changes.
+          function onPlayerStateChange(event) {
+            var time, rate, remainingTime;
+            clearTimeout(stopPlayTimer);
+            if (event.data == YT.PlayerState.PLAYING) {
+              time = player.getCurrentTime();
+              // Add .4 of a second to the time in case it's close to the current time
+              // (The API kept returning ~9.7 when hitting play after stopping at 10s)
+              if (time + .4 < stopPlayAt) {
+                rate = player.getPlaybackRate();
+                remainingTime = (stopPlayAt - time) / rate;
+                stopPlayTimer = setTimeout(pauseVideo, remainingTime * 1000);
+              }
+            }
+          }
+          function pauseVideo() {
+            player.pauseVideo();
+          }
+
+
 $(document).ready(function(){
-   
+
 $("#buttonPrint").click(function(){
     
     var Nome = $("#Nome").val();
